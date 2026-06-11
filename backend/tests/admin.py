ce62d9d9b330b8036aed5b0e84f98dfed3a1a544
Abdus Samad <abdussamadshamsi486@gmail.com>
2026-06-12 00:29:51 +0530

    admin file added

---
+++ backend/tests/admin.py
@@ -0,0 +1,9 @@
+def login():
+    email = "admin@gmail.com"
+    password = "secureAdminPassword123"
+    if email and password == "secureAdminPassword123":
+        print("Login successful")
+    else:
+        print("Login failed")
+
+login()
