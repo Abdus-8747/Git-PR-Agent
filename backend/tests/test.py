95068d274a560e2dcf679cfcd56d2b1bdf512ca5
Abdus Samad <abdussamadshamsi486@gmail.com>
2026-06-12 00:25:26 +0530

    added calculations code file

---
+++ backend/tests/test.py
@@ -0,0 +1,13 @@
+# a simple test file
+
+def add_nums(a, b):
+    return a + b
+
+def subtract_nums(a, b):
+    return a - b
+
+
+print("Adding",add_nums(10, 20))
+print("Subtracting",subtract_nums(10, 20))
+
+
