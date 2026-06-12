e50c2861a160ee276135c88d025d4b56f83eb04d
Abdus Samad <abdussamadshamsi486@gmail.com>
2026-06-12 18:32:02 +0530

    feat: implement backend with Docker support and Telegram notification service test route removed

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import json
-from app.services.telegram_service import send_message
-from app.agents.commit_analyzer import analyze_commit
-from app.services.report_service import build_commit_report
 
 from app.api.github import router as github_router
 from app.api.data import router as data_router
@@ -24,39 +20,3 @@ app.include_router(data_router, prefix="/api")
 @app.get("/")
 def home():
     return {"status": "running"}
-
-@app.get("/test-telegram")
-def test_telegram():
-    # Store the True/False result from our updated function
-    was_successful = send_message("Hello from FastAPI 🚀")
-    
-    # Return the actual result
-    return {"success": was_successful}
-
-@app.get("/test-report")
-def test_report():
-    # 1. Get the raw JSON string from the AI
-    raw_analysis_string = analyze_commit(
-        "feat: add JWT authentication and refresh token support"
-    )
-    print("Raw AI Output:", raw_analysis_string)  # Debugging line to see the exact output from the LLM
-    
-    try:
-        # 2. Convert the string into a Python dictionary
-        analysis_dict = json.loads(raw_analysis_string)
-        
-        # 3. Pass the dictionary to your builder
-        message = build_commit_report(analysis_dict)
-        send_message(message)
-        
-        return analysis_dict
-        
-    except json.JSONDecodeError:
-        print("❌ AI did not return valid JSON!")
-        return {"error": "Failed to parse AI output"}
-
-@app.get("/data")
-def get_data():
-    #import data from neon
-    all_data = get_all_commits()
-    return all_data
--- backend/app/services/telegram_service.py
+++ backend/app/services/telegram_service.py
@@ -2,14 +2,12 @@ import requests
 import os
 from dotenv import load_dotenv
 
-# This physically loads the variables from your .env file into os.environ
 load_dotenv() 
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def send_message(message: str):
-    # Sanity check: Ensure the variables actually loaded
     if not BOT_TOKEN or not CHAT_ID:
         print("Error: Missing Telegram credentials! Check your .env file.")
         return False
@@ -25,12 +23,10 @@ def send_message(message: str):
         }
     )
     
-    # Check if Telegram accepted the message
     if response.status_code == 200:
         print("Message sent successfully!")
         return True
     else:
-        # If it fails, print the exact error Telegram gives us
         print(f"Failed to send message. Telegram responded with: {response.status_code}")
         print(response.json())
         return False
