06d17915abe2aa286b0311787baf3b8b8af3f8ee
Abdus Samad <abdussamadshamsi486@gmail.com>
2026-06-13 12:12:15 +0530

    email service updated

--- backend/tests/email.py
+++ backend/tests/email.py
@@ -1,13 +1,17 @@
 import smtplib
+import os
+from dotenv import load_dotenv
 
-ADMIN_EMAIL_ADDRESS = "admin@gmail.com"
-ADMIN_EMAIL_PASS = "Rshabhedbjh27"
+load_dotenv()
+
+ADMIN_EMAIL_ADDRESS = os.getenv("ADMIN_EMAIL_ADDRESS")
+ADMIN_EMAIL_PASS = os.getenv("ADMIN_EMAIL_PASS")
 
 def send_email(email: str):
-    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    with smtplib.SMTP(ADMIN_EMAIL_ADDRESS, 587) as server:
         server.starttls()
         server.login(ADMIN_EMAIL_ADDRESS, ADMIN_EMAIL_PASS)
-        server.sendmail(ADMIN_EMAIL_ADDRESS, email, "Hello")
+        server.sendmail(ADMIN_EMAIL_ADDRESS, email, "Hello from Admin! Your request has been processed successfully.")
     print("Email sent")
 
 send_email("user1@gmail.com")
